a0a84da334604f934bfbb56067a0f338f4dacb52
bibisixtynine <85257463+bibisixtynine@users.noreply.github.com>
2022-12-31 11:35:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# svelte-codemirror6-clock
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sveltejs-kit-template-default-cn5mid)
---
+++ src/lib/Editor.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import CodeMirror from "svelte-codemirror-editor";
+    import { javascript } from "@codemirror/lang-javascript";
+
+    let value = "";
+    let counter = 0
+
+    setInterval( ()=> {
+        counter++
+    },1000)
+</script>
+
+<div class="title">CodeMirror6 Editor {counter}</div>
+
+<CodeMirror bind:value lang={javascript()} />
+
+<style>
+    .title {
+        text-align: center;
+    }
+</style>
